5670ea81184b8e1b6569089705e52ed695c20602
Illia-Dovgalenko <illia.dovgalenko@stu.ocsb.ca>
2026-03-27 00:26:43 -0400

    distance using a sonar

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Illia
+ * Created on: March 2026
+ * This program the distance using a sonar
 */
 
-basic.showString('Hello, World!')
+let distance = 5
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.showNumber(distance)
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    distance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    basic.showNumber(distance)
+    basic.showIcon(IconNames.Happy)
+})
